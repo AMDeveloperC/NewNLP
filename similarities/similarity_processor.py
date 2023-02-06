77b01f6ee8495350506fdca34e1e1b3da2df7ecc
AMDeveloperC <alessandro.mer.89@gmail.com>
2023-02-06 20:35:58 +0100

    Add the missing similarity_processor.py

---
+++ similarities/similarity_processor.py
@@ -0,0 +1,23 @@
+
+from gensim import similarities
+
+class SimilarityProcessor:
+    def __del__(self):
+        print("LSI ending")
+
+    def __init__(self, model, dictionary, corpus):
+        self.model = model
+        self.dictionary = dictionary
+        self.corpus = corpus
+
+    def compute_similarity(self):
+        self.index = similarities.SparseMatrixSimilarity(self.corpus, num_features=len(self.dictionary))
+
+    def process_query(self, query):
+        query_document = query.split()
+        query_bow = self.dictionary.doc2bow(query_document)
+        sims = self.index[self.model[query_bow]]
+        documents_id = []
+        for document_number, score in sorted(enumerate(sims), key=lambda x: x[1], reverse=True):
+            documents_id.append(document_number)
+        return sims, documents_id
